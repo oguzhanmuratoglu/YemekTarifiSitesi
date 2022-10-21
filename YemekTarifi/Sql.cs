47f395972247d62dbda12a9a051c8cec99e1d187
Oğuzhan Muratoğlu <116174285+oguzhanmuratoglu@users.noreply.github.com>
2022-10-21 15:39:51 +0300

    Update Sql.cs

--- YemekTarifi/Sql.cs
+++ YemekTarifi/Sql.cs
@@ -8,16 +8,11 @@ namespace YemekTarifi
 {
     public class Sql
     {
-        //public static SqlConnection cnn() 
-        //{
-        //    SqlConnection conn = new SqlConnection("workstation id=YemekTarifi14.mssql.somee.com;packet size=4096;user id=oguzhan14_SQLLogin_1;pwd=8w7qjb9quv;data source=YemekTarifi.mssql.somee.com;persist security info=False;initial catalog=YemekTarifi");
-        //    conn.Open();
-        //    return conn;
-        //}
+        
 
         public static SqlConnection cnn()
         {
-            SqlConnection conn = new SqlConnection(@"Data Source=MONSTER\SQLEXPRESS;Initial Catalog=Dbo_YemekTarifi;Integrated Security=True");
+            SqlConnection conn = new SqlConnection("workstation id=YemekTarifi.mssql.somee.com;packet size=4096;user id=oguzhan14_SQLLogin_1;pwd=8w7qjb9quv;data source=YemekTarifi14.mssql.somee.com;persist security info=False;initial catalog=YemekTarifi");
             conn.Open();
             return conn;
         }
